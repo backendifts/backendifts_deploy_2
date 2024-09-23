eb5c4762934f1ed063144c9678588dd0b155f1f9
backendifts <backendifts@gmail.com>
2024-09-23 20:41:54 -0300

    Sever Express Basico

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+// Ruta principal - para que responda algo
+app.get("/",(req, res)=>{
+    res.send("Back End Deploy !!!!");
+});
+
+// const PORT = 3000; // Manera Local
+// console.log(process.env.PORT)
+
+const PORT = process.env.PORT || 3000;
+//console.log(PORT);
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
